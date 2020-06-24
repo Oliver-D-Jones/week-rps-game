c99403cb1446166ab67025a08253292fdf2767f7
OliverDJones <oliverjones.boise@gmail.com>
2020-06-24 16:31:22 -0600

    Final

--- main.js
+++ main.js
@@ -1,18 +1,12 @@
 function play(choice){
-  let compChoiceString = "";
-  let playerChoiceString = ""
-  let choiceNumber = parseInt(choice);
-  let objPlayerOut=document.getElementById('playerChoice')
-  let objCompOut=document.getElementById('compChoice')
-  let objWinnerOut=document.getElementById('winner')
-  
+  let choiceNumber = parseInt(choice);  
   let random = Math.floor(Math.random()*3);
-  compChoiceString = random == 0 ? "Rocks" : random == 1 ? "Paper" : "Scissors";
-  playerChoiceString = choiceNumber == 0 ? "Rocks" : choiceNumber == 1 ? "Paper" : "Scissors";
+  let compChoiceString = random == 0 ? "Rocks" : random == 1 ? "Paper" : "Scissors";
+  let playerChoiceString = choiceNumber == 0 ? "Rocks" : choiceNumber == 1 ? "Paper" : "Scissors";
 
-  objCompOut.textContent=compChoiceString;
-  objPlayerOut.textContent=playerChoiceString;
-  objWinnerOut.textContent = winner(choiceNumber,random);
+  document.getElementById('compChoice').textContent=compChoiceString;
+  document.getElementById('playerChoice').textContent=playerChoiceString;
+  document.getElementById('winner').textContent = winner(choiceNumber,random);
 }
 function winner(p,c){
  return (p+1)%3 == c ? "You Lose." : p == c ? "It's a Tie." : "You Win!";
